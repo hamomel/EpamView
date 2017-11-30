95d3ff306a07ee10cbcff4980b226b9065e57c7a
hamom <hamomel@gmail.com>
2017-11-30 10:48:33 +0400

    RippleTextView implemented

--- app/src/main/java/com/hamom/epamview/ui/custom_views/RippleTextView.java
+++ app/src/main/java/com/hamom/epamview/ui/custom_views/RippleTextView.java
@@ -2,14 +2,18 @@ package com.hamom.epamview.ui.custom_views;
 
 import android.animation.ValueAnimator;
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.content.ContextCompat;
 import android.util.AttributeSet;
-import android.util.Log;
+import android.util.DisplayMetrics;
+import android.view.Gravity;
 import android.view.MotionEvent;
+import android.view.WindowManager;
+import android.view.animation.AccelerateInterpolator;
 
 import com.hamom.epamview.R;
 
@@ -19,57 +23,115 @@ import com.hamom.epamview.R;
 
 public class RippleTextView extends android.support.v7.widget.AppCompatTextView {
 
-    private Paint mPaint;
+    private static final int DEFAULT_CIRCLE_WIDTH_DP = 2;
+    private static final int RIPPLE_ALPHA = 50;
+    private int mSize;
+    private int mMainColor;
+    private int mRippleColor;
+    private Paint mRipplePaint;
     private Paint mCirclePaint;
-    private float mRadius;
+    private float mCircleRadius;
     private float mRippleRadius;
+    private float mCircleWidth;
     private ValueAnimator mAnimator;
-    private ValueAnimator mRverseAnimator;
 
     public RippleTextView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        TypedArray array = context.obtainStyledAttributes(attrs, R.styleable.RippleTextView);
 
-        mPaint = new Paint();
-        mPaint.setColor(ContextCompat.getColor(context, R.color.colorAccent));
-        mPaint.setStyle(Paint.Style.FILL);
+        float circleWidth;
+        try {
+            int defColor = ContextCompat.getColor(context, android.R.color.white);
+            mMainColor = array.getColor(R.styleable.RippleTextView_mainColor, defColor);
+            mRippleColor = array.getColor(R.styleable.RippleTextView_rippleColor, defColor);
+            circleWidth = array.getDimension(R.styleable.RippleTextView_circleWidth, 0);
+        } finally {
+            array.recycle();
+        }
+
+        float scale = getScale(context);
+        mCircleWidth = circleWidth > 0 ? circleWidth : DEFAULT_CIRCLE_WIDTH_DP * scale;
+    }
+
+    private float getScale(Context context) {
+        DisplayMetrics metrics = new DisplayMetrics();
+        ((WindowManager) context.getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay().getMetrics(metrics);
+        return metrics.density;
+    }
+
+    private void init() {
+        setTextColor(mMainColor);
+        setGravity(Gravity.CENTER);
+        setClickable(true);
+        setTextSize(mSize / 6);
+
+        mCircleRadius = mSize / 2 - mCircleWidth;
+
+        mRipplePaint = new Paint();
+        mRipplePaint.setColor(mRippleColor);
+        mRipplePaint.setStyle(Paint.Style.FILL);
+        mRipplePaint.setAlpha(RIPPLE_ALPHA);
 
         mCirclePaint = new Paint();
-        mCirclePaint.setColor(ContextCompat.getColor(context, android.R.color.black));
+        mCirclePaint.setColor(mMainColor);
         mCirclePaint.setStyle(Paint.Style.STROKE);
-        mCirclePaint.setStrokeWidth(4);
+        mCirclePaint.setStrokeWidth(mCircleWidth);
 
+        mAnimator = ValueAnimator.ofFloat(0, mCircleRadius);
+        mAnimator.setInterpolator(new AccelerateInterpolator());
+        mAnimator.addUpdateListener(getListener());
+    }
 
+    public int getMainColor() {
+        return mMainColor;
+    }
+
+    public void setMainColor(int mainColor) {
+        mMainColor = mainColor;
+    }
+
+    public int getRippleColor() {
+        return mRippleColor;
+    }
+
+    public void setRippleColor(int rippleColor) {
+        mRippleColor = rippleColor;
+    }
+
+    public float getCircleWidth() {
+        return mCircleWidth;
+    }
+
+    public void setCircleWidth(float circleWidth) {
+        mCircleWidth = circleWidth;
     }
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-        float height = MeasureSpec.getSize(heightMeasureSpec);
-        mRadius = height/2 - 2;
-        mAnimator = ValueAnimator.ofFloat(0, mRadius);
-        mAnimator.addUpdateListener(getListener());
+        int height = MeasureSpec.getSize(heightMeasureSpec);
+        int width = MeasureSpec.getSize(widthMeasureSpec);
 
-        mRverseAnimator = ValueAnimator.ofFloat(mRippleRadius, 0);
-        mRverseAnimator.setDuration(500);
-        mRverseAnimator.addUpdateListener(getListener());
+        int min = Math.min(height, width);
+        mSize = min;
+        int w = resolveSizeAndState(min, widthMeasureSpec, 0);
+        int h = resolveSizeAndState(min, heightMeasureSpec, 0);
+        setMeasuredDimension(w, h);
+        init();
     }
 
     @NonNull
     private ValueAnimator.AnimatorUpdateListener getListener() {
-        return new ValueAnimator.AnimatorUpdateListener() {
-            @Override
-            public void onAnimationUpdate(ValueAnimator animation) {
-                mRippleRadius = (float) animation.getAnimatedValue();
-                Log.d("RippleTV in listener", "" + mRippleRadius);
-                invalidate();
-            }
+        return animation -> {
+            mRippleRadius = (float) animation.getAnimatedValue();
+            invalidate();
         };
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
-        canvas.drawCircle(getWidth()/2, getHeight()/2, mRippleRadius, mPaint);
-        canvas.drawCircle(getWidth()/2, getHeight()/2, mRadius, mCirclePaint);
+        canvas.drawCircle(getWidth()/2, getHeight()/2, mRippleRadius, mRipplePaint);
+        canvas.drawCircle(getWidth()/2, getHeight()/2, mCircleRadius, mCirclePaint);
         super.onDraw(canvas);
     }
 
@@ -80,15 +142,10 @@ public class RippleTextView extends android.support.v7.widget.AppCompatTextView 
                 mAnimator.start();
                 return true;
             case MotionEvent.ACTION_UP:
-                Log.d("RippleTV :", "" + mRippleRadius);
                 mAnimator.reverse();
-//                if (mAnimator.isRunning()) {
-//                } else {
-//                    mRverseAnimator.start();
-//                }
                 return true;
-
+                default:
+                    return super.onTouchEvent(event);
         }
-        return super.onTouchEvent(event);
     }
 }
